5c2b34462c69a45336f2380c0f0bce27a7a199fb
Unknown <dperezg1@eafit.edu.co>
2018-04-13 18:37:29 -0500

    datos personales
    Primeras cuatro preguntas

---
+++ lib/RegistroHV.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RegistroHV extends StatefulWidget {
+  @override
+  _RegistroHVState createState() => new _RegistroHVState();
+}
+
+class _RegistroHVState extends State<RegistroHV>{
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold();
+  }
+}
