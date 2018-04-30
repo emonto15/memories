538e82bbe6e477b3d403226422827b8b2a3879f3
Edwin Montoya Jaramillo <emonto15@eafit.edu.co>
2018-04-29 21:03:06 -0500

    Adding support for relase on github on the dev branch

--- set_tags.sh
+++ set_tags.sh
@@ -12,6 +12,7 @@ if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
 
       git config --global user.email "emonto15@eafit.edu.co"
       git config --global user.name "Edwin Montoya Jaramillo"
+      git checkout dev
 
       # Add tag and push to master.
       git tag -a v${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."
