0e82eae6be5792ad8d4d4b30935f12d459b6f2af
Diego Perez <dperezg1@eafit.edu.co>
2018-05-19 19:12:58 -0500

    Reporte arreglado

--- lib/Reporte.dart
+++ lib/Reporte.dart
@@ -23,13 +23,13 @@ class AreasBarra {
             r: color.red, g: color.green, b: color.blue, a: color.alpha);
 }
 
-class Emociones{
+class Gustos{
   final String emocion;
   final int promedioSemanal;
   final charts.Color color;
 
 
-  Emociones(this.emocion, this.promedioSemanal, Color color) 
+  Gustos(this.emocion, this.promedioSemanal, Color color) 
       : this.color = charts.Color(
             r: color.red, g: color.green, b: color.blue, a: color.alpha);
       
@@ -58,7 +58,7 @@ class _ReporteState extends State<Reporte> {
 
 
   int memoriaBarras = 0;
-  int orientacionBarras = 90;
+  int orientacionBarras = 0;
   int juicioBarras = 0;
 
 //variales diagrama de lineas
@@ -71,10 +71,25 @@ class _ReporteState extends State<Reporte> {
   List<AreasLine> dataJuicioLine = <AreasLine>[inicioJuicio];
   List<AreasLine> dataOrientacionLine = <AreasLine>[inicioOrientacion];
 
-//variables diagrama de barra emociones
-  int felicidadBarras =50;
-  int neutralBarras = 20;
-  int tristeBarras = 80;
+
+  //variables diagrama de barra gustos
+  //memoria
+  int cualNoEsBarras = 0;
+  int cualEsBarras = 0;
+  int infoBarras = 0;
+
+  //orientacion
+  int fechasBarras = 0;
+  int situacionesBarras= 0;
+
+  //Juicio
+  int calculoBarras = 0;
+  int semejanzasBarras = 0;
+  int diferenciasBarras = 0;
+  int secuenciasBarras = 0;
+
+
+
 
    
       
@@ -82,6 +97,7 @@ class _ReporteState extends State<Reporte> {
 void initState() {
     super.initState();
     _getAreasData();
+    _getGustosData();
   
   }
 
@@ -92,16 +108,23 @@ void initState() {
     //datos para las barras 
     var data = [
       new AreasBarra('Memoria', memoriaBarras, Colors.purple),
-      new AreasBarra('Juicio', orientacionBarras, Colors.deepPurple),
-      new AreasBarra('Orientacion', juicioBarras, Colors.purpleAccent),
+      new AreasBarra('Juicio', orientacionBarras, Colors.purple[300]),
+      new AreasBarra('Orientacion', juicioBarras, Colors.purple[700]),
     ];
 
 
   //datos para el diagrama de barras de las emociones semanal
-  var emocionesData = [
-    new Emociones("feliz", felicidadBarras, Colors.blue),
-    new Emociones("Neutral", neutralBarras, Colors.green),
-    new Emociones("Triste", tristeBarras, Colors.red),
+  var gustosData = [
+    new Gustos("Cual No es", cualNoEsBarras, Colors.purple[50]),
+    new Gustos("Cual es", cualEsBarras, Colors.purple[100]),
+    new Gustos("Información", infoBarras, Colors.purple[200]),
+    new Gustos("Fechas", fechasBarras, Colors.purple[300]),
+    new Gustos("Situaciones", situacionesBarras, Colors.purple[400]),
+    new Gustos("Calculo", calculoBarras, Colors.purple),
+    new Gustos("Semejanzas", semejanzasBarras, Colors.purple[600]),
+    new Gustos("Diferencias", diferenciasBarras, Colors.purple[700]),
+    new Gustos("Secuencias", secuenciasBarras, Colors.purple[800])
+    
 
   ];
 
@@ -152,11 +175,11 @@ void initState() {
 
 var seriesEmocines = [
       new charts.Series(
-        domainFn: (Emociones emocionData, _) => emocionData.emocion,
-        measureFn: (Emociones emocionData, _) => emocionData.promedioSemanal,
-        colorFn: (Emociones emocionData, _) => emocionData.color,
-        id: 'Emociones',
-        data: emocionesData,
+        domainFn: (Gustos gustosData, _) => gustosData.emocion,
+        measureFn: (Gustos gustosData, _) => gustosData.promedioSemanal,
+        colorFn: (Gustos gustosData, _) => gustosData.color,
+        id: 'Gustos',
+        data: gustosData,
       ),
     ];
 
@@ -207,13 +230,13 @@ var seriesEmocines = [
         padding: new EdgeInsets.only(top: 40.0, left: 40.0, right: 40.0),
         child: new ListView(
           children: <Widget>[
-            new Text("Reporte semanal porcentual de que tan bien esta el paciente en cada Area: ",
+            new Text("Reporte semanal porcentual por Area: ",
             style: new TextStyle(fontSize: 18.0,fontWeight: FontWeight.bold ,)),
             chartWidget,
-            new Text("Reporte historico porcentual de que tan bien está el paciente en cada Area: ",
+            new Text("Reporte historico porcentual por Area: ",
             style: new TextStyle(fontSize: 18.0,fontWeight: FontWeight.bold ,)),
             chartWidget2,
-            new Text("Reporte semanal porcentual de emociones durante las actividades : ",
+            new Text("Reporte semanal porcentual por Subareas : ",
             style: new TextStyle(fontSize: 18.0,fontWeight: FontWeight.bold ,)),
             chartWidgetEmociones
           
@@ -250,11 +273,11 @@ var seriesEmocines = [
       print(nuevoMapa);
       setState(() {
               var tam = nuevoMapa["test"].length;
-      memoriaBarras = (100.0 - (nuevoMapa["test"][tam-1]["paciente"]["memoria"]*33.333333)).toInt();      
-      orientacionBarras = (100 - (nuevoMapa["test"][tam-1]["paciente"]["orientacion"]*33.333333)).toInt();
-      juicioBarras = (100 - (nuevoMapa["test"][tam-1]["paciente"]["juicio"]*33.333333)).toInt();
+      memoriaBarras = (100.0 - (nuevoMapa["test"][tam-1]["memoria"]*33.333333)).toInt();      
+      orientacionBarras = (100 - (nuevoMapa["test"][tam-1]["orientacion"]*33.333333)).toInt();
+      juicioBarras = (100 - (nuevoMapa["test"][tam-1]["juicio"]*33.333333)).toInt();
      
-      print(nuevoMapa["test"][1]["fecha"]);
+      
       for (int i = 0; i < tam; i++){
         var year = DateTime.parse(nuevoMapa["test"][i]["fecha"]).year;
         var month = DateTime.parse(nuevoMapa["test"][i]["fecha"]).month;
@@ -262,13 +285,57 @@ var seriesEmocines = [
 
 
 
-        dataMemoriaLine.add(new AreasLine(new DateTime(year,month,day), (100.0 - nuevoMapa["test"][i]["paciente"]["memoria"]*33.333333).toInt(), Colors.purple));
-        dataJuicioLine.add(new AreasLine(new DateTime(year,month,day), (100.0 - nuevoMapa["test"][i]["paciente"]["juicio"]*33.333333).toInt(), Colors.deepPurple));
-        dataOrientacionLine.add(new AreasLine(new DateTime(year,month,day), (100.0 - nuevoMapa["test"][i]["paciente"]["orientacion"]*33.333333).toInt(), Colors.purpleAccent));
+        dataMemoriaLine.add(new AreasLine(new DateTime(year,month,day), (100.0 - nuevoMapa["test"][i]["memoria"]*33.333333).toInt(), Colors.purple));
+        dataJuicioLine.add(new AreasLine(new DateTime(year,month,day), (100.0 - nuevoMapa["test"][i]["juicio"]*33.333333).toInt(), Colors.purple[300]));
+        dataOrientacionLine.add(new AreasLine(new DateTime(year,month,day), (100.0 - nuevoMapa["test"][i]["orientacion"]*33.333333).toInt(), Colors.purple[700]));
 
       }
       
             });
+      
+
+      print(memoriaBarras);
+
+    } catch (err) {
+      print(err);
+    }
+  }
+
+   Future<Null> _getGustosData() async {
+    try {
+      var user = new Map();
+      var nuevoMapa = new Map();
+      //String URL="http://192.168.1.81:3000";
+
+      //user["google_id"] = "1";
+      user["google_id"] = widget.googleId;
+
+      final String requestBody = json.encode(user);
+      print(requestBody);
+      HttpClientRequest request =
+          await httpClient.postUrl(Uri.parse(URL + '/users/report'))
+            ..headers.add(HttpHeaders.ACCEPT, ContentType.JSON)
+            ..headers.contentType = ContentType.JSON
+            ..headers.contentLength = requestBody.length
+            ..headers.chunkedTransferEncoding = false;
+      request.write(requestBody);
+      HttpClientResponse response = await request.close();
+      nuevoMapa = json.decode(await response.transform(utf8.decoder).join());
+
+      print(nuevoMapa);
+      setState(() {
+      cualNoEsBarras = nuevoMapa["1"]["1"];
+      cualEsBarras = nuevoMapa["1"]["2"];
+      infoBarras = nuevoMapa["1"]["3"];
+
+      fechasBarras = nuevoMapa["2"]["1"];
+      situacionesBarras = nuevoMapa["2"]["2"];
+
+      calculoBarras = nuevoMapa["3"]["1"];
+      semejanzasBarras = nuevoMapa["3"]["2"];
+      diferenciasBarras = nuevoMapa["3"]["3"];
+      secuenciasBarras = nuevoMapa["3"]["4"];
+      });
       
 
       print(memoriaBarras);
